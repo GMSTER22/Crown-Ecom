94a5f51d1169b1aaae3381ea34e16f1f2b2887d3
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-05-05 23:08:15 -0400

    started Crown  e-commerce website by creating category container

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import CategoryContainer from "./components/category-container/category-container.component";
+
+const App = () => {
+  
+  return <CategoryContainer />
+  
+}
+
+export default App;
